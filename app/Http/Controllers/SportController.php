5045a3889bf0862b0f453606228bc96386ad7d2f
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-06-08 17:19:54 +0200

    Api request works

--- app/Http/Controllers/SportController.php
+++ app/Http/Controllers/SportController.php
@@ -14,72 +14,24 @@ class SportController extends Controller
      */
     public function index()
     {
-        //
+      $sports = Sport::all();
+
+      return response()->json([
+        'data' => $sports,
+        'success' => true,
+      ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+      $data = $request->all();
+
+      //tutte le operazioni di store;
+
+      return response()->json([
+        'message' => 'Sport saved correctly',
+        'success' => true,
+      ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Sport  $sport
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Sport $sport)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Sport  $sport
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Sport $sport)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Sport  $sport
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Sport $sport)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Sport  $sport
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Sport $sport)
-    {
-        //
-    }
 }
--- database/seeds/SportSeeder.php
+++ database/seeds/SportSeeder.php
@@ -22,6 +22,5 @@ class SportSeeder extends Seeder
 
         $sport->save();
       }
-
     }
 }
